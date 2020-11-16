3dba689c82a9c368de593b6661de1c756c0104ad
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 17:32:36 +0200

    add function of checking existing user

--- run.py
+++ run.py
@@ -15,3 +15,38 @@ def create_user(name, password):
     new_user = User(name,password)
 
     return new_user
+def save_users(user):
+    '''
+    Function to save a user account
+    Args:
+        user : the user account to be saved
+    '''
+
+    user.save_user()
+
+def check_existing_users(name):
+    '''
+    Function that checks if a user account name already exists
+    Args:
+        name : the user account name
+    '''
+
+    return User.user_exist(name)
+
+def user_log_in(name, password):
+    '''
+    Function that allows a user to log into their credential account
+    Args:
+        name : the name the user used to create their user account
+        password : the password the user used to create their user account
+    '''
+    log_in = User.log_in(name, password)
+    if log_in != False:
+        return User.log_in(name, password)
+
+def display_users():
+    '''
+    Function that returns all the saved users 
+    '''
+
+    return User.display_user()
