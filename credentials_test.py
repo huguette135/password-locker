0781717114cdf1cc3c20ae029287b651a0134a7f
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 16:56:46 +0200

    add a class of testcredential

---
+++ credentials_test.py
@@ -0,0 +1,9 @@
+import unittest
+from credential import Credential
+
+class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines test cases for the Credential Class behaviours
+    Args:
+        unittest.TestCase : Test case class that helps create test cases
+    '''
