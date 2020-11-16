869fc7c124167d3753160b809852fc81d8f3b5b0
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 17:07:43 +0200

    add test the credention exist

--- credentials_test.py
+++ credentials_test.py
@@ -7,6 +7,7 @@ class TestCredential(unittest.TestCase):
     Args:
         unittest.TestCase : Test case class that helps create test cases
     '''
+
     def setUp(self):
         '''
         Set up method to run before each test case
@@ -21,7 +22,8 @@ class TestCredential(unittest.TestCase):
         '''
 
         Credential.credential_list = []
-        def test_init(self):
+
+    def test_init(self):
         '''
         Test case to test if the object is initialised properly
         '''
@@ -39,7 +41,7 @@ class TestCredential(unittest.TestCase):
 
         self.assertEqual( len(Credential.credential_list), 1 )
 
-        def test_save_multiple_credentials(self):
+    def test_save_multiple_credentials(self):
         '''
         Test case to test if you can save multiple objects to credential list
         '''
@@ -52,6 +54,52 @@ class TestCredential(unittest.TestCase):
         test_credential.save_credential()
 
         self.assertEqual( len(Credential.credential_list), 2)
+
+    def test_generate_password(self):
+        '''
+        Test case to test if a user can log into their credentials
+        '''
+        
+        generated_password = self.new_credential.generate_password()
+
+        self.assertEqual( len(generated_password), 8 )   
+    
+    def test_display_credential(self):
+        '''
+        Test case to test if a user can see a list of all the credentials saved
+        '''
+
+        # Save the new credential
+        self.new_credential.save_credential()
+
+        test_credential = Credential("doe2","Facebook","facebook17")
+
+        test_credential.save_credential()
+
+        test_credential = Credential("doe2","Yahoo","yahoo17")
+
+        test_credential.save_credential()
+        
+        self.assertEqual( len(Credential.display_credential("doe2")) , 2 )
+        
+    def test_credential_exist(self):
+        
+        '''
+        Test to check if we can return a boolean if we can't find the credential
+        '''
+
+        # Save the new credential
+        self.new_credential.save_credential()
+
+        test_credential = Credential("doe2","Facebook","facebook17")
+
+        test_credential.save_credential()
+        
+        # use contact exist method
+        credential_exists = Credential.credential_exist("Facebook")
+        
+        self.assertTrue(credential_exists)
         
 
-
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
