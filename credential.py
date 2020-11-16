d89228a1b1b6c16f33f5256a2c735e966645839d
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 16:54:56 +0200

    add some change

--- credential.py
+++ credential.py
@@ -10,7 +10,8 @@ class Credential:
     '''
     Class that generates instances of a users credentials
     '''
-     # Empty list of credentials
+
+    # Empty list of credentials
     credential_list = []
 
     def __init__(self, user_password, credential_name, credential_password):
@@ -24,18 +25,21 @@ class Credential:
         self.user_password = user_password
         self.credential_name = credential_name
         self.credential_password = credential_password
-def save_credential(self):
-'''
-Method that saves a user's credentials to credential list
-'''
-Credential.credential_list.append(self)
-@classmethod
+
+    def save_credential(self):
+        '''
+        Method that saves a user's credentials to credential list
+        '''
+        Credential.credential_list.append(self)
+
+    @classmethod
     def generate_password(cls):
         '''
         Method that generates a random alphanumeric password
         '''
         # Length of the generated password
         size = 8
+
         # Generate random alphanumeric 
         alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
 
@@ -43,7 +47,8 @@ Credential.credential_list.append(self)
         password = ''.join( choice(alphanum) for num in range(size) )
         
         return password
-        @classmethod
+
+    @classmethod
     def display_credential(cls,password):
         '''
         Method that returns the credential list
@@ -57,6 +62,7 @@ Credential.credential_list.append(self)
                 user_credential_list.append(credential)
 
         return user_credential_list
+    
     def delete_credential(self):
         '''
         Method that deletes a user's credentials from credential list
@@ -79,7 +85,8 @@ Credential.credential_list.append(self)
         for credential in cls.credential_list:
             if credential.credential_name == name:
                 return credential
-                @classmethod
+                
+    @classmethod
     def credential_exist(cls, name):
         
         '''
