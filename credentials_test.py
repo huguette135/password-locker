4200e1de33ecf0f65d5a7e86b06cd1fbd4f1c0d6
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 17:00:30 +0200

    add function of initialisation and for saving credential

--- credentials_test.py
+++ credentials_test.py
@@ -21,3 +21,20 @@ class TestCredential(unittest.TestCase):
         '''
 
         Credential.credential_list = []
+        def test_init(self):
+        '''
+        Test case to test if the object is initialised properly
+        '''
+        self.assertEqual( self.new_credential.user_password, "doe")
+        self.assertEqual( self.new_credential.credential_name, "Yahoo" )
+        self.assertEqual( self.new_credential.credential_password, "yahoo17" )
+
+    def test_save_credential(self):
+        '''
+        Test case to test if the user object is saved into the user list
+        '''
+
+        # Save the new credential
+        self.new_credential.save_credential()
+
+        self.assertEqual( len(Credential.credential_list), 1 )
