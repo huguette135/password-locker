0d494efeb9e8a9759d9c0804d9201138016d6c18
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-11-16 16:37:43 +0200

    add changes

--- credential.py
+++ credential.py
@@ -10,3 +10,14 @@ class Credential:
     '''
     Class that generates instances of a users credentials
     '''
+     # Empty list of credentials
+    credential_list = []
+
+    def __init__(self, user_password, credential_name, credential_password):
+        '''
+        __init__ method to define the properties of a User object
+        Args:
+            user_password : password of the user
+            credential_name : name of an account
+            credential_password : password for the account
+        '''
